9018f39b3d5e634091caa487fe080ae4ffddaf85
fahim <fsyusufz@uwaterloo.ca>
2020-10-29 17:51:05 -0400

    using subp w/o make

--- test.py
+++ test.py
@@ -1,4 +1,21 @@
 import subprocess
-list_files = subprocess.run(["make"])
-print("The exit code was: %d" % list_files.returncode)
+
+tex="sample.tex"
+pdf="sample.pdf"
+svg="sample.svg"
+png="sample.png"
+aux="sample.aux"
+log="sample.log"
+
+pdflatex = subprocess.run(["pdflatex", tex, pdf])
+pdfcrop  = subprocess.run(["pdfcrop", pdf, pdf])
+pdf2svg  = subprocess.run(["pdf2svg", pdf, svg])
+inkscape = subprocess.run(["inkscape", "--export-dpi=300", svg, "--export-png="+png])
+clean    = subprocess.run(["rm", "-rf", pdf, svg, log, aux])
+
+print("The exit code was: %d" % pdflatex.returncode)
+print("The exit code was: %d" % pdfcrop.returncode)
+print("The exit code was: %d" % pdf2svg.returncode)
+print("The exit code was: %d" % inkscape.returncode)
+print("The exit code was: %d" % clean.returncode)
 
